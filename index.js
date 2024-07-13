3d7c3e0147cfd432ce259fe6301e2f9d338a2530
Inamov Asilbek <playerallgames007@gmail.cpm>
2024-07-13 16:05:06 +0500

    Fix

---
+++ index.js
@@ -0,0 +1,21 @@
+const TelegramBot = require("node-telegram-bot-api")
+
+const token = "7406327164:AAF5Z3SZu66sQV25Sn1o9WVeTsdllg2o4Fc"
+
+const bot = new TelegramBot(token, { polling: true })
+
+bot.on('message', async (msg) => {
+    console.log(msg);
+
+    const text = msg.text
+    const chatId = msg.chat.id
+
+    if (text === "/start") {
+        return bot.sendMessage(chatId, `Assalomu alaykum ${msg.from?.first_name + msg.from?.last_name}. Hisoblagich botga hush kelibsiz :) \n \n Agar botga tushunmasangiz => \n \n 1) Menu tugmasini bosing \n 2) help sozini boshing`)
+        // const a = text.typeof == Number
+    }
+    if (text === "/help") {
+        return bot.sendMessage(chatId, `Bu botga \n  \n 1-(raqam), \n 2-(raqam), \n 3-(Ishora (+, -, *, /)) \n \n 4) Sizga javob keladi`)
+    }
+    
+})
